56eb32d07494b707b37deec1969195569063f30f
bathys <68154148+bathys@users.noreply.github.com>
2024-05-25 04:41:45 +0900

    init

---
+++ nixos/packages.nix
@@ -0,0 +1,33 @@
+{pkgs, ... }:
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+  # Desktop apps
+
+
+  # Coding stuff
+
+  # CLI utils
+
+  # GUI utils
+
+  # Wayland stuff
+
+  # WMs and studd
+
+  # Sound
+
+  # Screenshotting
+
+  # Others
+
+
+  ];
+
+
+
+
+}
