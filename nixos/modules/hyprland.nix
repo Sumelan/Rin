78e9aa32fd8fe0cb37d6b0e92a1ed2e62a944a37
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-25 11:06:02 +0900

    Update hyprland.nix

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -2,5 +2,14 @@
   programs = {
     hyprland.enable = true;
     xwayland.enable = true;
+    dconf.enable = true;
+    waybar.enable = true;
+  };
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+	    xdg-desktop-portal-hyprland
+    ];
   };
 }
