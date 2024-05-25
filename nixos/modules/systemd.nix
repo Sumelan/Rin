3dda57a03155a06096e61482dad98d4576d974b7
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-25 11:15:53 +0900

    Create systemd.nix

---
+++ nixos/modules/systemd.nix
@@ -0,0 +1,10 @@
+{
+  systemd.services = {
+	  NetworkManager-wait-online.enable = false;
+	  firewalld.enable = true;
+	  power-profiles-daemon = {
+		  enable = true;
+		  wantedBy = [ "multi-user.target" ];
+  		};
+  }; 
+}
