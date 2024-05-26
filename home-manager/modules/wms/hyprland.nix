a09a9019673a7fcdc2ff94ffc10c79efd53c4b4b
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-26 23:44:26 +0900

    Update hyprland.nix

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -114,65 +114,65 @@
         enable_stdout_logs = true
       }
 
-      input  {
+      input {
         kb_layout = us
 
         follow_mouse = 1
+        sensitivity = 0
+        accel_profile = false
 
         touchpad {
-          natural_scroll = false
+          natural_scroll = true
           middle_button_emulation = true
           clickfinger_behavior = true
         }
-
-        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
-
-        gestures {
-          workspace_swipe = true
-          workspace_swipe_fingers = 3
-       }
-
-        general {
-          gaps_in = 4
-          gaps_out = 4
-          border_size = 2
-          col.active_border = rgba(${theme.base0A}ff)
-          col.inactive_border = rgb();
-
-        layout = "dwindle"
-        no_cursor_warps = false
       }
 
-        decoration {
-          rounding = 0;
-          blur {
-            enabled = true
-            size = 2
-            passes = 3
-          new_optimizations = true
-          }
-          drop_shadow = false
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(${theme.base02}ff)
-        }
+      gestures {
+        workspace_swipe = true
+        workspace_swipe_fingers = 3
+      }
 
-        animations {
-          enable = true
-          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-        # bezier = myBezier, 0.33, 0.82, 0.9, -0.08
-          animation = windows,     1, 7,  myBezier
-          animation = windowsOut,  1, 7,  default, popin 80%
-          animation = border,      1, 10, default
-          animation = borderangle, 1, 8,  default
-          animation = fade,        1, 7,  default
-          animation = workspaces,  1, 6,  default
+      general {
+        gaps_in = 4
+        gaps_out = 4
+        border_size = 2
+        col.active_border = rgba(${theme.base0A}ff)
+        col.inactive_border = rgb();
+
+        layout = dwindle
+        allow_tearing = true
+      }
+
+      decoration {
+        rounding = 0
+        blur {
+          enabled = true
+          size = 2
+          passes = 3
+          xray = true
+          new_optimizations = true
         }
+        drop_shadow = false
+        shadow_range = 4
+        shadow_render_power = 3
+        col.shadow = rgba(${theme.base02}ff)
+      }
+
+      animations {
+        enabled = true
+        bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+        animation = windows, 1, 7, myBezier
+        animation = windowsOut, 1, 7, default, popin 80%
+        animation = border, 1, 10, default
+        animation = borderangle, 1, 8, default
+        animation = fade, 1, 7, default
+        animation = workspaces, 1, 6, default
       }
 
       dwindle {
-        pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-        preserve_split = true # you probably want this
+        pseudotile = true
+        preserve_split = true
       }
 
       master {
@@ -180,11 +180,7 @@
       }
 
       misc {
-        animate_manual_resizes = true
-        animate_mouse_windowdragging = true
-        enable_swallow = true
-        render_ahead_of_time = false
-        disable_hyprland_logo = true
+        force_default_wallpaper = -1
       }
  '';
  };
