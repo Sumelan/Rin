c4643835e492e3292b4af9516f6610711e46710b
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-26 15:01:03 +0900

    Update hyprland.nix

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -113,9 +113,6 @@
         gestures {
           workspace_swipe = true
           workspace_swipe_fingers = 3
-          workspace_swipe_invert = false
-          workspace_swipe_distance = 200
-          workspace_swipe_forever = true
        }
 
         general {
