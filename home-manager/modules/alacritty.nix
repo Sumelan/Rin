e60b37d3019b4d261b1e827ff2403bd322cedb47
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-26 14:46:41 +0900

    Update alacritty.nix

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -5,7 +5,7 @@
       window.opacity = 0.95;
 
       font = {
-        size = 13.0;
+        size = 10.0;
         # draw_bold_text_with_bright_colors = true;
         normal = {
           family = "JetBrains Mono";
