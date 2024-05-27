5d787e68faccb2eb5ccca730c289eca3cce4da3d
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-27 22:41:07 +0900

    Update waybar.nix

--- home-manager/modules/wms/waybar.nix
+++ home-manager/modules/wms/waybar.nix
@@ -239,7 +239,7 @@
 		"weeks-pos" = ""; # display week number. right/left
 		"on-scroll" = 1;
 		"format" = {
-			"months" = "<span color='#ffead3'><b>{  }</b></span>";
+			"months" = "<span color='#ffead3'><b>{}</b></span>";
 			"days" = "<span color='#ecc6d9'><b>{}</b></span>";
 			"weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
 			"weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
