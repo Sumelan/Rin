b02fa359428266d10003a7e0204dc4a15be716ed
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-28 01:00:57 +0900

    Update layout.nix

--- home-manager/modules/wms/wlogout/layout.nix
+++ home-manager/modules/wms/wlogout/layout.nix
@@ -1,33 +1,42 @@
-{
-    label = "lock";
-    action = "$HOME/.config/hypr/scripts/LockScreen.sh";
-    text = "Lock";
-    keybind = "l";
-
-    label = "reboot";
-    action = "systemctl reboot";
-    text = "Reboot";
-    keybind = "r";
-
-    label = "shutdown";
-    action = "systemctl poweroff";
-    text = "Shutdown";
-    keybind = "s";
-
+{ 
+  programs.wlogout.layout = [
+    {
+      label = "lock";
+      action = "$HOME/.config/hypr/scripts/LockScreen.sh";
+      text = "Lock";
+      keybind = "l";
+    }
+    {
+      label = "reboot";
+      action = "systemctl reboot";
+      text = "Reboot";
+      keybind = "r";
+    }
+    {
+      label = "shutdown";
+      action = "systemctl poweroff";
+      text = "Shutdown";
+      keybind = "s";
+    }
+    {
      label = "logout";
      action = "loginctl kill-session $XDG_SESSION_ID";
      text = "Logout";
      keybind = "e";
-
-    label = "suspend";
-    action = "systemctl suspend";
-    text = "Suspend";
-    keybind = "u";
-  
-    label = "hibernate";
-    action = "systemctl hibernate";
-    text = "Hibernate";
-    keybind = "h";
+    }
+    {
+     label = "suspend";
+     action = "systemctl suspend";
+     text = "Suspend";
+     keybind = "u";
+    }
+    {
+     label = "hibernate";
+     action = "systemctl hibernate";
+     text = "Hibernate";
+     keybind = "h";
+    }
+   ];
 }
 
 
