9cc03faa0d2bbd298eaa48edbe5c25552e601ed7
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-30 17:33:28 +0900

    Update systemd.nix

--- nixos/modules/systemd.nix
+++ nixos/modules/systemd.nix
@@ -7,26 +7,4 @@
 		  wantedBy = [ "multi-user.target" ];
   		};
   }; 
-
-# Masking sleep, hibernate, suspend.. etc
-  systemd = {
-    targets = {
-      sleep = {
-	enable = false;
-	unitConfig.DefaultDependencies = "no";
-      };
-      suspend = {
-	enable = false;
-	unitConfig.DefaultDependencies = "no";
-      };
-      hibernate = {
-	enable = false;
-      unitConfig.DefaultDependencies = "no";
-      };
-     "hybrid-sleep" = {
-	enable = false;
-	unitConfig.DefaultDependencies = "no";
-      };
-    };
-  };
 }
