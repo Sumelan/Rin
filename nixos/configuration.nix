379a6512b8d0fb0a3c838693b13b89b6da58e834
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-30 14:06:31 +0900

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,9 +49,6 @@
   services = {
     thermald.enable = true;
     tlp.enable = true;
-    udev.extraRules = ''
-      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
-    '';
   };
 
 # Sleep, Hibernate, etc.
