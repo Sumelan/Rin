a023f634b4337295acc37a1614c747d05a847067
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-30 13:42:35 +0900

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,7 +50,7 @@
     thermald.enable = true;
     tlp.enable = true;
     udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"
+      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
     '';
   };
 
