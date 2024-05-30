036732ceb42ba74549746017a64e2f2744db0ca2
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-05-30 17:42:02 +0900

    Update services.nix

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -1,12 +1,28 @@
 {
-services = {
-	  gvfs.enable = true;
-	  tumbler.enable = true;	
-	  udev.enable = true;
-	  envfs.enable = true;
-	  dbus.enable = true;
-	  fwupd.enable = true;
-	  upower.enable = true;
+  services = {
+	gvfs.enable = true;
+	tumbler.enable = true;	
+	udev.enable = true;
+	envfs.enable = true;
+	dbus.enable = true;
+	fwupd.enable = true;
+	upower.enable = true;
+# Display Manager
+    	displayManager.sddm = {
+        	enable = true;
+        	autoNumlock = true;
+        	wayland.enable = true;
+        	theme = "sugar-dark";
+      	};
+    	xserver = {
+      		enable = true;
+      		desktopManager.cinnamon.enable = false;
+        	xkb = {
+          		layout = "us";
+          		variant = "";
+       		};
+     	};
+
 ### Laptop
     	thermald.enable = true;
     	tlp.enable = true;
