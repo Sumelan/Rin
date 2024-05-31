0c2a4edf66741257816bd477d0e3a3194f7dc07f
bathys <68154148+bathys@users.noreply.github.com>
2024-06-01 03:29:39 +0900

    init

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,13 +8,13 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = lib.mkDefault "UUID=699d5f20-89a1-4ed2-b92d-60c191386b78";
+    { device = lib.mkDefault "UUID=6dd81528-acd9-4024-bca3-89964177202f";
       fsType = "bcachefs";
     };
 
@@ -25,7 +25,7 @@
     };
 
   fileSystems."/boot" =
-    { device = lib.mkDefault "/dev/disk/by-uuid/5492-956A";
+    { device = lib.mkDefault "/dev/disk/by-uuid/8984-704F";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
@@ -37,9 +37,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
