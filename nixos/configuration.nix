b5507a2e4b5dd6aa4295e8a53d108664ca78a304
installer <installer@gmail.com>
2024-06-01 01:53:42 +0900

    init

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,8 +4,6 @@
   imports = [
       ./hardware-configuration.nix
       ./packages.nix
-      ./sddm-sugar-dark.nix
-      ./sddm-tokyo-night.nix
       ./amd-drivers.nix
       ./modules/bundle.nix
       ./disko-config.nix
