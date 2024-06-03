88d7fcfbdd77241cd2226083802041dd6c4fb35e
bathys <68154148+bathys@users.noreply.github.com>
2024-06-03 23:11:39 +0900

    init

--- nixos/modules/nextcloud.nix
+++ nixos/modules/nextcloud.nix
@@ -1,8 +1,8 @@
 { self, config, lib, pkgs, ... }:
 {
   services.nextcloud = {
-    enable = true;
-    hostName = "nextcloud.cherryblossom.freeddns.org";
+    enable = false;
+    hostName = "sakurapc.freeddns.org";
   ## Need to manually increment with every major upgrade.
     package = pkgs.nextcloud29;
   # Let NixOS install and configure the database automatically.
@@ -21,8 +21,16 @@
       inherit calendar contacts mail notes onlyoffice tasks;
     };
 
-    settings = {
-      overwriteProtocol = "https";
+    settings = let
+      prot = "https"; # or http
+      host = "127.0.0.1";
+      dir = "/nextcloud";
+    in {
+      overwriteprotocol = prot;
+      overwritehost = host;
+      overwritewebroot = dir;
+      overwrite.cli.url = "${prot}://${host}${dir}/";
+      htaccess.RewriteBase = dir;
       defaultPhoneRegion = "JP";
     };
     config = {
@@ -30,19 +38,70 @@
       adminuser = "bathys";
       adminpassFile = "/var/nextcloud-admin-pass";
     };
+    extraOptions.enabledPreviewProviders = [
+      "OC\\Preview\\BMP"
+      "OC\\Preview\\GIF"
+      "OC\\Preview\\JPEG"
+      "OC\\Preview\\Krita"
+      "OC\\Preview\\MarkDown"
+      "OC\\Preview\\MP3"
+      "OC\\Preview\\OpenDocument"
+      "OC\\Preview\\PNG"
+      "OC\\Preview\\TXT"
+      "OC\\Preview\\XBitmap"
+      "OC\\Preview\\HEIC"
+    ];
   };
 
   services.nginx.virtualHosts = {
     ${config.services.nextcloud.hostName} = {
       forceSSL = true;
       enableACME = true;
+      listen = [
+      { 
+       addr = "127.0.0.1";
+       port = 8080; # NOT an exposed port
+      }
+      ];
+    };
+  };
+
+  services.nginx.virtualHosts."localhost" = {
+    "^~ /.well-known" = {
+            priority = 9000;
+            extraConfig = ''
+              absolute_redirect off;
+              location ~ ^/\\.well-known/(?:carddav|caldav)$ {
+                return 301 /nextcloud/remote.php/dav;
+              }
+              location ~ ^/\\.well-known/host-meta(?:\\.json)?$ {
+                return 301 /nextcloud/public.php?service=host-meta-json;
+              }
+              location ~ ^/\\.well-known/(?!acme-challenge|pki-validation) {
+                return 301 /nextcloud/index.php$request_uri;
+              }
+              try_files $uri $uri/ =404;
+            '';
+    };
+    "/nextcloud/" = {
+            priority = 9999;
+            extraConfig = ''
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-NginX-Proxy true;
+              proxy_set_header X-Forwarded-Proto http;
+              proxy_pass http://127.0.0.1:8080/; # tailing / is important!
+              proxy_set_header Host $host;
+              proxy_cache_bypass $http_upgrade;
+              proxy_redirect off;
+            '';
     };
   };
 
   security.acme = {
     acceptTerms = true;
-    certs = {
-      ${config.services.nextcloud.hostName}.email = "nishimura7802@gmail.com";
+    certs.${config.services.nextcloud.hostName} = {
+      email = "nishimura7802@gmail.com";
     };
   };
 }
