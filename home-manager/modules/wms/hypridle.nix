b5a37ecf37bd53a17ed296ac58cf38af4d0e12ee
bathys <68154148+bathys@users.noreply.github.com>
2024-06-08 18:01:23 +0900

    init

--- home-manager/modules/wms/hypridle.nix
+++ home-manager/modules/wms/hypridle.nix
@@ -2,10 +2,10 @@
   services.hypridle = {
     enable = true;
     settings = {
-      geberal = {
+      general = {
         lock_cmd = "pidof hyprlock || hyprlock";       # avoid starting multiple hyprlock instances.
         before_sleep_cmd = "loginctl lock-session";    # lock before suspend.
-        after_sleep_cmd = "hyprctl dispath dpms on";  # to avoid having to press a key twice to run on the dispaly.
+        after_sleep_cmd = "hyprctl dispath dpms on";  # to avoid having to press a key twice to run on the display.
       };
 
       listener = [
@@ -25,9 +25,9 @@
           on-timeout = "loginctl lock-session";  # lock screen when timeout has passed.
         } 
         {
-         # timeout = 680;                            #10.5 min.
-         # on-timeout = "hyprctl dispatch dpms off";  #screen off when timeout has passed.
-         # on-resume = "hyprctl dispatch dpms on";    #screen on when activity is detected after timeout has fired.
+           timeout = 680;                            #10.5 min.
+           on-timeout = "hyprctl dispatch dpms off";  #screen off when timeout has passed.
+           on-resume = "hyprctl dispatch dpms on";    #screen on when activity is detected after timeout has fired.
         }
         {
           timeout = 18000;                  # 5 h.
