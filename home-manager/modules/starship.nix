24c30bf25ab8ee64dd36378e5b882f70be5fb3e6
Bathys Scarf <bathys@proton.me>
2024-06-14 11:30:17 +0900

    Create starship.nix

---
+++ home-manager/modules/starship.nix
@@ -0,0 +1,12 @@
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        error_symbol = "¬◇";
+        success_symbol = "∴";
+      };
+    };
+  };
+}
