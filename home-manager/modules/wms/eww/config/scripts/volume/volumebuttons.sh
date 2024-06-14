86ff0a5a80d9deff4dc45ab8fe2e8a800a275fe6
Bathys Scarf <bathys@proton.me>
2024-06-14 15:43:42 +0900

    Update volumebuttons.sh

--- home-manager/modules/wms/eww/config/scripts/volume/volumebuttons.sh
+++ home-manager/modules/wms/eww/config/scripts/volume/volumebuttons.sh
@@ -2,7 +2,7 @@
 
 trap "echo 'Script terminated'; exit" SIGTERM SIGINT
 
-iconBasePath="/home/bathys/dotfiles/home-manager/modules/eww/config/images"
+iconBasePath="/home/bathys/Rin/home-manager/modules/eww/config/images"
 
 volumeDownIconNormal="minus.png"
 volumeDownIconMin="minusmax.png"
