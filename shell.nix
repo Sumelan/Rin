ee0faebcc7777353b8861e299528210333b24d59
Bathys Scarf <bathys@proton.me>
2024-06-15 22:23:15 +0900

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    gobject-introspection
-  ];
-  buildInputs = with pkgs; [
-    gtk3
-    (python3.withPackages (p: with p; [
-      pygobject3
+# shell.nix
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      # select Python packages here
+      python-pkgs.pandas
+      python-pkgs.requests
+      python-pkgs.numpy
+      python-pkgs.pygobject3
+      python-pkgs.gst-python
     ]))
   ];
-}
-
-
