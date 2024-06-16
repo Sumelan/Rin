4e8f6035ba5b4bf9b9f56c470303d2b92fd02146
Bathys Scarf <bathys@proton.me>
2024-06-17 07:48:29 +0900

    Update papirus.nix

--- nixos/icons/papirus.nix
+++ nixos/icons/papirus.nix
@@ -1,4 +1,4 @@
-{ lib, stdenvNoCC, fetchFromGitHub, gtk3, pantheon, breeze-icons, gnome-icon-theme, hicolor-icon-theme, papirus-folders, color ? null }:
+{ lib, stdenvNoCC, fetchFromGitHub, gtk3, pantheon, breeze-icons, gnome-icon-theme, hicolor-icon-theme, papirus-folders, color ? null, ... }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "papirus-icon-theme";
