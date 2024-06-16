013ce69a25bb76b32007b9890f7b4a4fea5e9d7d
Bathys Scarf <bathys@proton.me>
2024-06-17 05:10:35 +0900

    Delete home-manager/modules/wms/eww/config/scripts directory

--- home-manager/modules/wms/eww/config/scripts/cava/cava_pipe.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash
-
-confPath="$HOME/.config/eww/scripts/cava/config"
-
-# Main
-cava -p $confPath | while read -r line; do echo $line| sed -e 's/;/,/g'; done | while read -r line; do echo "["`echo ${line/%?/}`"]"; done
