e5c3540a1ac8dc19c129b026676df0b38f7de83c
Bathys Scarf <bathys@proton.me>
2024-06-17 06:16:25 +0900

    Update swaylock.nix

--- home-manager/modules/wms/swaylock/swaylock.nix
+++ home-manager/modules/wms/swaylock/swaylock.nix
@@ -1,51 +1,42 @@
-{
-  programs.swaylock = {
-    enable = true;
+{ colors, pkgs }: {
+  programs.swaylock = with colors;{
+    package = pkgs.swaylock-effects;
     settings = {
-      image = "/home/bathys/.config/swaylock/lockscreen/lockscreen.png";
-      indicator-idle-visible = false;
-      indicator-radius = 100;
-      indicator-thickness = 20;
-      show-failed-attempts = true;
-
-      bs-hl-color = "#f38ba8";
-      color = "#1e1e2e28";
-      key-hl-color = "#cba6f7";
-
-      caps-lock-bs-hl-color = "#f38ba8";
-      caps-lock-key-hl-color = "#cba6f7";
-
-      inside-color = "#1e1e2e28";
-      inside-clear-color = "#1e1e2e";
-      inside-caps-lock-color = "#1e1e2e";
-      inside-ver-color = "#1e1e2e";
-      inside-wrong-color = "#1e1e2e";
-
-      line-color = "#1e1e2e";
-      line-clear-color = "#1e1e2e";
-      line-caps-lock-color = "#1e1e2e";
-      line-ver-color = "#1e1e2e";
-      line-wrong-color = "#1e1e2e";
-
-      ring-color = "#11111b";
-      ring-clear-color = "#11111b";
-      ring-caps-lock-color = "#11111b";
-      ring-ver-color = "#11111b";
-      ring-wrong-color = "#11111b";
-
-      separator-color = "#00000000";
-
-      text-color = "#cdd6f4";
-      text-clear-color = "#cdd6f4";
-      text-caps-lock-color = "#cdd6f4";
-      text-ver-color = "#cdd6f4";
-      text-wrong-color = "#cdd6f4";
+      clock = true;
+      color = "00000000";
+      font = "Product Sans";
+      show-failed-attempts = false;
+      indicator = true;
+      indicator-radius = 220;
+      indicator-thickness = 25;
+      line-color = "#${background}";
+      ring-color = "${mbg}";
+      inside-color = "#${background}";
+      key-hl-color = "#${accent}";
+      separator-color = "00000000";
+      text-color = "#${foreground}";
+      text-caps-lock-color = "";
+      line-ver-color = "#${accent}";
+      ring-ver-color = "#${accent}";
+      inside-ver-color = "#${background}";
+      text-ver-color = "#${foreground}";
+      ring-wrong-color = "#${color9}";
+      text-wrong-color = "#${color9}";
+      inside-wrong-color = "#${background}";
+      inside-clear-color = "#${background}";
+      text-clear-color = "#${foreground}";
+      ring-clear-color = "#${color5}";
+      line-clear-color = "#${background}";
+      line-wrong-color = "#${background}";
+      bs-hl-color = "#${accent}";
+      line-uses-ring = false;
+      grace = 0;
+      grace-no-mouse = true;
+      grace-no-touch = true;
+      datestr = "%d.%m";
+      fade-in = "0.1";
+      ignore-empty-password = true;
     };
   };
 
-  # Place Files Inside Home Directory
-  home.file.".config/swaylock/lockscreen" = {
-    source = ./lockscreen;
-    recursive = true;
-  };
 }
