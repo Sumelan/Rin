79854d4e454a550603e54f28f945d0a9685331c4
bathys <bathys@proton.me>
2024-06-18 22:50:55 +0900

    init

--- home-manager/bin/rofiscripts/powermenu.sh
+++ home-manager/bin/rofiscripts/powermenu.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-  chosen=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n  Log Out\n  Lock" | rofi -dmenu -i)
 
-  case "$chosen" in
-  	"  Power Off") poweroff ;;
-    "  Restart") reboot ;;
-    "  Suspend") systemctl suspend-then-hibernate ;;
-  	"  Hibernate") systemctl hibernate ;;
-  	"󰗽  Log Out") loginctl kill-user $USER ;;
-  	"  Lock") hyprlock ;;
-  	*) exit 1 ;;
-  esac
+chosen=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n󰗽   Log Out\n  Lock" | rofi -dmenu -i)
+
+case "$chosen" in
+	"  Power Off") poweroff ;;
+  "  Restart") reboot ;;
+  "  Suspend") systemctl suspend-then-hibernate ;;
+	"  Hibernate") systemctl hibernate ;;
+ 	"󰗽  Log Out") loginctl kill-user $USER ;;
+ 	"  Lock") hyprlock ;;
+ 	*) exit 1 ;;
+esac
--- home-manager/bin/rofiscripts/screenshot.sh
+++ home-manager/bin/rofiscripts/screenshot.sh
@@ -1,70 +1,70 @@
 #!/usr/bin/env bash
 
-  active="Active window"
-  screen="All visible outputs"
-  output="Currently active output"
-  area="Select an area"
+active="Active window"
+screen="All visible outputs"
+output="Currently active output"
+area="Select an area"
 
-  copy="Copy to clipboard"
-  save="Save file"
+copy="Copy to clipboard"
+save="Save file"
 
-  tmp_filename="/tmp/screenshot_$(date +%d)-$(date +%m)-$(date +%y)_$(date +%T).png"
+tmp_filename="/tmp/screenshot_$(date +%d)-$(date +%m)-$(date +%y)_$(date +%T).png"
 
-  # Function taken directly from grimshot
-  # https://github.com/swaywm/sway/blob/master/contrib/grimshot
-  target_directory() {
-    echo ~/Pictures/Screenshots
-  }
+# Function taken directly from grimshot
+# https://github.com/swaywm/sway/blob/master/contrib/grimshot
+target_directory() {
+  echo ~/Pictures/Screenshots
+}
 
-  # Rofi CMD
-  rofi_cmd() {
-  	rofi -dmenu \
-  		-mesg "Screenshot" -theme-str 'inputbar { enabled: false; }'
-  }
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-mesg "Screenshot" -theme-str 'inputbar { enabled: false; }'
+}
 
-  # Pass variables to rofi dmenu
-  run_rofi() {
-  	echo -e "$active\n$screen\n$output\n$area\n$window" | rofi_cmd
-  }
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$active\n$screen\n$output\n$area\n$window" | rofi_cmd
+}
 
-  # Mode selection
-  mode_cmd() {
-    rofi -dmenu
-  }
+# Mode selection
+mode_cmd() {
+  rofi -dmenu
+}
 
-  # Ask to select mode
-  select_mode() {
-  	echo -e "$copy\n$save" | mode_cmd
-  }
+# Ask to select mode
+select_mode() {
+	echo -e "$copy\n$save" | mode_cmd
+}
 
-  menu_option="$(run_rofi)"
-  if [[ ! -z "$menu_option" ]]
+menu_option="$(run_rofi)"
+if [[ ! -z "$menu_option" ]]
+then
+  case $menu_option in
+  	$active)
+  	grimblast --notify save active $tmp_filename > /dev/null 2>&1
+  		;;
+  	$screen)
+  	grimblast --notify save screen $tmp_filename > /dev/null 2>&1
+  		;;
+  	$output)
+  	grimblast --notify save output $tmp_filename > /dev/null 2>&1
+  		;;
+  	$area)
+  	grimblast --notify save area $tmp_filename > /dev/null 2>&1
+  		;;
+  esac
+  mode="$(select_mode)"
+  if [[ ! -z "$mode" ]]
   then
-  	case $menu_option in
-  		$active)
-  		grimblast --notify save active $tmp_filename > /dev/null 2>&1
+  	case $mode in
+  		$copy)
+  		wl-copy --type image/png < $tmp_filename
   			;;
-  		$screen)
-  		grimblast --notify save screen $tmp_filename > /dev/null 2>&1
-  			;;
-  		$output)
-  		grimblast --notify save output $tmp_filename > /dev/null 2>&1
-  			;;
-  		$area)
-  		grimblast --notify save area $tmp_filename > /dev/null 2>&1
+  		$save)
+  		cp $tmp_filename $(target_directory)
   			;;
   	esac
-  	mode="$(select_mode)"
-  	if [[ ! -z "$mode" ]]
-  	then
-  		case $mode in
-  			$copy)
-  			wl-copy --type image/png < $tmp_filename
-  				;;
-  			$save)
-  			cp $tmp_filename $(target_directory)
-  				;;
-  		esac
-  	fi
+  fi
   	rm $tmp_filename
   fi
