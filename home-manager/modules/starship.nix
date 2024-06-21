772deeaee158267ee161026d30f088133840e1d5
Bathys Scarf <bathys@proton.me>
2024-06-22 07:42:29 +0900

    Update starship.nix

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -1,12 +1,5 @@
 {
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      character = {
-        error_symbol = "¬◇";
-        success_symbol = "∴";
-      };
-    };
-  };
+	programs.starship = {
+		enable = true;
+	};
 }
