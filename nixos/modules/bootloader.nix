d87ff6604aa11dbcbeb28d498897aa06f1fa2479
Bathys Scarf <bathys@proton.me>
2024-06-22 07:07:13 +0900

    Update bootloader.nix

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,25 +1,31 @@
-{pkgs, ... }:
+{pkgs, config, ... }:
 {
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_8;
-  boot.kernelParams = [  ];
-  boot.loader = {
-   # systemd-boot.enable = true;
-    timeout = 5;
-    efi.efiSysMountPoint = "/boot";
-    grub = {
-      enable = true;
-      efiSupport = true;
-      efiInstallAsRemovable = true; #Otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generate.
-      devices = [ "nodev" ];
-      extraEntriesBeforeNixOS = true;
-      extraEntries = ''
-        menuentry "Reboot" {
-          reboot
-        }
-        menuentry "Poweroff" {
-          halt
-        }
-        '';
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    # This is for OBS Virtual Cam Support
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # Needed For Some Steam Games
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
     };
+    # Bootloader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    # Make /tmp a tmpfs
+    tmp = {
+      useTmpfs = false;
+      tmpfsSize = "30%";
+    };
+    # Appimage Support
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+    plymouth.enable = true;
   };
 }
