7cd405ca5fe31c4307a5253b730ca20c747dacbc
Bathys Scarf <bathys@proton.me>
2024-06-22 07:09:38 +0900

    Update env.nix

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,6 +1,6 @@
 {
   environment.variables = {
-    EDITOR = "nvim";
+    EDITOR = "neovim";
     RANGER_LOAD_DEFAULT_RC = "FALSE";
     QT_QPA_PLATFORMTHEME = "qt5ct";
     GSETTINGS_BACKEND = "keyfile";
