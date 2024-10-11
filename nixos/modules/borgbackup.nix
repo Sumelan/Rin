2c552f4aa8a0a0b5d6b6eaa3f017ccdbebeec3a3
Su melan <bathys@proton.me>
2024-10-12 06:56:03 +0900

    only backup /var/lib/nextcloud.

--- nixos/modules/borgbackup.nix
+++ nixos/modules/borgbackup.nix
@@ -1,28 +1,11 @@
 {
   services.borgbackup.jobs."borgbase" = {
     paths = [
-      "/var/lib"
-      "/srv"
-      "/home"
+      "/var/lib/nextcloud"
     ];
     exclude = [
-      # Cache.
-      "/home/sumelan/.cache"
-
-      # very large paths
-      "/var/lib/docker"
-      "/var/lib/systemd"
-      "/var/lib/libvirt"
-
-      # NOT much frequently changed.
-      "/var/lib/audiobookshelf"
-    
-      # temporary files created by cargo and `go build`
-      "**/target"
-      "/home/*/go/bin"
-      "/home/*/go/pkg"
     ];
-    repo = "ef79t855@ef79t855.repo.borgbase.com:repo";
+    repo = "u36h67h7@u36h67h7.repo.borgbase.com:repo";
     encryption = {
       mode = "repokey-blake2";
       passCommand = "cat /root/borgbackup/passphrase";
