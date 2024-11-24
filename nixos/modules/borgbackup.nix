c1d669a695cbce79689095312c4dfa5c28c67496
Su melan <bathys@proton.me>
2024-11-24 17:31:06 +0900

    rewrite borgbase.nix

--- nixos/modules/borgbackup.nix
+++ nixos/modules/borgbackup.nix
@@ -1,16 +1,11 @@
 {
-  services.borgbackup.jobs."borgbase-daily" = {
+  services.borgbackup.jobs."borgbase" = {
     paths = [
-      "/var/lib/nextcloud/data/sumelan/files/Documents"
-      "/var/lib/nextcloud/data/sumelan/files/Music"
-      "/var/lib/nextcloud/data/sumelan/files/Notes"
-      "/var/lib/nextcloud/data/sumelan/files/Pictures"
-      "/var/lib/nextcloud/data/sumelan/files/Videos"
-      "/var/lib/nextcloud/data/sumelan/files/VRChat"
+      "/var/lib/nextcloud"
     ];
     exclude = [
     ];
-    repo = "pyur7fry@pyur7fry.repo.borgbase.com:repo";
+    repo = "tn864p7j@tn864p7j.repo.borgbase.com:repo";
     encryption = {
       mode = "repokey-blake2";
       passCommand = "cat /root/borgbackup/passphrase";
@@ -18,37 +13,5 @@
     environment.BORG_RSH = "ssh -i /root/borgbackup/ssh_key";
     compression = "auto,lzma";
     startAt = "daily";
-  };
-
-  services.borgbackup.jobs."borgbase-weekly" = {
-    paths = [
-      "/var/lib/nextcloud/data/sumelan/files/Resource"
-    ];
-    exclude = [
-    ];
-    repo = "f92yij06@f92yij06.repo.borgbase.com:repo";
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /root/borgbackup/passphrase";
-    };
-    environment.BORG_RSH = "ssh -i /root/borgbackup/ssh_key";
-    compression = "auto,lzma";
-    startAt = "weekly";
-  };
-
-  services.borgbackup.jobs."borgbase-montyly" = {
-    paths = [
-      "/var/lib/nextcloud/data/sumelan/files/Archive"
-    ];
-    exclude = [
-    ];
-    repo = "rdp4gk60@rdp4gk60.repo.borgbase.com:repo";
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /root/borgbackup/passphrase";
-    };
-    environment.BORG_RSH = "ssh -i /root/borgbackup/ssh_key";
-    compression = "auto,lzma";
-    startAt = "monthly";
   };
 }
